14a8d8f6eafc0c6d2aa0b81b8322685ad2df811e
soooyaya <hsoo0531@naver.com>
2022-11-22 23:11:31 +0900

    BINGO

--- bingoBoard.c
+++ bingoBoard.c
@@ -7,27 +7,44 @@ static int bingoBoard[N_SIZE][N_SIZE];
 static int numberStatus[N_SIZE*N_SIZE];
 
 
+int bingo_checkNum(int selNum)
+{
+	if (numberStatus[selNum -1] == BINGONUM_HOLE)
+	    return BINGO_NUMSTATUS_ABSENT; 
+	    
+	return BINGO_NUMSTATUS_PRESENT;
+}
 
 
 void bingo_init(void)
 {
-    int i, j;
-    int cnt = 1;
+    int i, j, k;
+    int randNum;  //랜덤 번호 (몇번째 숫자를 선택)
+	int maxNum = N_SIZE*N_SIZE;  //현재 남아있는 숫자
+	
+	for (i=0;i<N_SIZE*N_SIZE;i++)
+	   numberStatus[i] = BINGO_NUMSTATUS_ABSENT; 
     
 	for (i=0;i<N_SIZE;i++)
 	    for (j=0;j<N_SIZE;j++)
 	    {
-	    	if (cnt == 15)
+	    	randNum = rand()%maxNum;  //랜덤 숫자 선택 
+	    	
+	    	for (k=0;k<N_SIZE*N_SIZE;k++)  // 각 숫자에 대해 따져보기 
 	    	{
-	    	   bingoBoard[i][j] = BINGONUM_HOLE;
-	    	   numberStatus[cnt-1] = BINGONUM_HOLE;
-	    	   cnt++;
+	    	   if (numberStatus[k] == BINGO_NUMSTATUS_ABSENT)  // 숫자에 k+1이 아직 할당이 안되어 있다면 
+			   {
+                   if (randNum == 0) // k+1이 randNum 번째 숫자이면 
+				       break; 
+				   else
+				      randNum--; // 그렇지 않으면 하나 줄임 
+			   } 
 	        }
-	        else
-	        {
-			    numberStatus[cnt-1] = i*N_SIZE + j;
-			    bingoBoard[i][j] = cnt++;
-		    }
+	        //숫자 k+1을 할당
+			numberStatus[k] = i*N_SIZE + j;
+			bingoBoard[i][j] = k+1;
+			maxNum--;
+	        
 	    	
 		}
 }
@@ -36,7 +53,7 @@ void bingo_init(void)
 void bingo_print(void)
 {
 	int i,j;
-	printf("===========================\n");
+	printf("=============================\n");
 	for (i=0;i<N_SIZE;i++) {
 	    for (j=0;j<N_SIZE;j++)
 	    {
@@ -48,15 +65,93 @@ void bingo_print(void)
 		printf("\n");
 	}
 	
-	printf("========================\n\n");
+	printf("==========================\n\n");
 }
+
 
 void bingo_inputNum(int sel)
 {
+	int row, col;
+	row = numberStatus[sel-1]/N_SIZE;
+	col = numberStatus[sel-1]%N_SIZE;
 	
+	bingoBoard[row][col] = BINGONUM_HOLE;
+	numberStatus[sel -1] = BINGONUM_HOLE;
 }
+
 
 int bingo_countCompletedLine(void)
 {
+     int i, j;
+	 int cnt=0;
+	 int checkBingo;
+	 
+	 //row 
+     for (i=0;i<N_SIZE;i++)
+     {
+	    checkBingo=1;
+		for(j=0;j<N_SIZE;j++)
+     	   if (bingoBoard[i][j] > 0)
+     	   {
+			  checkBingo = 0;
+			  break;
+		   }
+			
+	    if (checkBingo == 1)
+         cnt ++;
+	 }
+	 
+	 
+//col
+   for (j=0;j<N_SIZE;j++)
+   {
+       checkBingo=1;
+       for (i=0;i<N_SIZE;i++)
+	       if (bingoBoard[i][j] > 0)
+           {
+			 checkBingo = 0;
+			 break;
+	       }
+	   
+	    if (checkBingo == 1)
+	      cnt ++;
+   }
+	 
+//diagonal
+   checkBingo=1;
+   for (i=0;i<N_SIZE;i++)
+   {
+      if (bingoBoard[i][i] > 0)
+	   {
+	      checkBingo = 0;
+		  break;
+	   }
+	}
+	if (checkBingo == 1)
+	  cnt ++;
+	  
+	  
+	checkBingo=1;
+	for (i=0;i<N_SIZE;i++)
+	{
+		if (bingoBoard[i][N_SIZE-i-1] > 0)
+		{
+			checkBingo = 0;
+			break;
+		}
+	}	 
+	if (checkBingo == 1)
+	  cnt ++;
+	  
 	
+	return cnt;
 }
+
+
+
+
+
+
+
+
+
--- main.c
+++ main.c
@@ -1,10 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include "bingoBoard.h"
+
+#define BINGO_RES_UNFINISHED           -1
+#define BINGO_RES_FINISHED              0
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
+int check_gameEnd(void)
+{
+	int res = BINGO_RES_UNFINISHED;
+	if (bingo_countCompletedLine() >= N_LINE)
+	    res = BINGO_RES_FINISHED;
+	    
+	return res; 
+}
+ 
+
+int get_number(void)
+{
+	int selNum=0;
+	
+	do {
+		printf("select a number : ");
+		scanf("%d", &selNum);
+		fflush(stdin);
+		
+		if (selNum < 1 || selNum > N_SIZE*N_SIZE || bingo_checkNum(selNum) == BINGO_NUMSTATUS_ABSENT)
+		{ 
+		    printf("%i is not on the board! select other one.\n", selNum);
+		}
+	} while (selNum < 1 || selNum > N_SIZE*N_SIZE || bingo_checkNum(selNum) == BINGO_NUMSTATUS_ABSENT);
+	
+	return selNum;
+}
+
+
 int main(int argc, char *argv[]) {
+	
+	int selNum=0;
 	
 	srand( (unsigned)(time(NULL)) );
 	
@@ -15,30 +50,30 @@ int main(int argc, char *argv[]) {
 	printf("**********************************************\n");
 	printf("==============================================\n");
 	
+	
 	//game
 	
 	bingo_init();
-	bingo_print();
-	bingo_inputNum(21);
-	bingo_print();
-	
-	//initialize bingo board
-    /*
-    while (game is not end) //줄 수가 N_BINGO보다 작음 
-    {
-        //bingo board print
-        
-		//print no. of completed line
-		
-		//select a number
-		
-		//update the board status    
-		
-    }
-    
-   */
+    //initialize bingo board
+	 
+	while (check_gameEnd() == BINGO_RES_UNFINISHED) // 줄수가 N_BINGO보다 작음 
+	{ 
+	  //bingo board print
+	  bingo_print();
+	 
+	  //print no. of completed line
+	  printf("No. of completed line : %i\n", bingo_countCompletedLine());
+	 
+	  //select a number
+	  selNum = get_number();
+	 
+	  //update the board status
+	  bingo_inputNum(selNum);
+	}
 	
 	
+	
+
 	
 	//ending
 	printf("\n\n\n\n\n\n\n\n\n");
@@ -49,8 +84,7 @@ int main(int argc, char *argv[]) {
 	printf("**********************************************\n");
 	printf("==============================================\n");
 	
-	
-	
+	bingo_print();
 	
 	return 0;
 }
